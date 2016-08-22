84866a308f9b83dc0eeda0a74aa313b9e08e8028
Richard Jiang Shenhao <richardejiang@hotmail.com>
2016-08-23 00:31:05 +0800

    Completed the Python code for processing XML files and form into txt files

---
+++ Python/Parser.py
@@ -0,0 +1,80 @@
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import re
+
+class XmlParser:
+	''' The general-purpose Xml parser '''
+	# fileName = ""
+	# rootTag = ""
+	# targetTags = []
+
+	def __init__(self, fileList, rootTag, targetTags):
+		self.fileList = fileList
+		self.rootTag = rootTag
+		self.targetTags = targetTags
+		self.content = ""
+
+	def parse(self):
+
+		count = 0
+		result = ""
+
+		for fileName in self.fileList:
+			DOMTree = xml.dom.minidom.parse(fileName)
+			collection = DOMTree.documentElement
+
+			#articles = collection.getElementsByTagName("article_rec")
+			articles = collection.getElementsByTagName(self.rootTag)
+			regexBracket = re.compile(r'<.*?>', re.IGNORECASE)
+			regexQuote = re.compile(r'\"', re.IGNORECASE)
+
+			for article in articles:
+
+				for tag in self.targetTags:
+					tagContents = article.getElementsByTagName(tag)
+					#print tagContent
+
+					if (tag != "article_publication_date"):
+						for tagContent in tagContents:
+							tagContent = re.sub(r'<.*?>', "", tagContent.childNodes[0].data)
+							tagContent = re.sub(r'\"', "", tagContent)
+							# tagContent = regexBracket.sub("", tagContent)
+							# tagContent = regexQuote.sub("", tagContent)
+							self.content += tagContent
+							self.content += " "
+
+					else:
+						for time in tagContents:
+							timeList = time.childNodes[0].data.split("-")
+							timing = timeList[len(timeList) - 1]
+							self.content = timing + " " + self.content
+				# titles = article.getElementsByTagName("title")
+				# abstracts = article.getElementsByTagName("par")
+				# timeStamp = article.getElementsByTagName("article_publication_date")
+
+				# for title in titles:
+				# 	title = regexBracket.sub("", title)
+				# 	title = regexQuote.sub("", title)
+				# 	self.content += title
+				# 	self.content += " "
+
+				# for abstract in abstracts:
+				# 	abstract = regexBracket.sub("", abstract)
+				# 	abstract = regexQuote.sub("", abstract)
+				# 	self.content += abstract
+				# 	self.content += " "
+
+				# for time in timeStamp:
+				# 	timeList = time.split('-')
+				# 	timing = timeList[len(timeList) - 1]
+				# 	self.content = timing + " " + self.content
+				if (len(self.content.split(" ")) <= 20):
+					continue
+
+				if self.content:
+					self.content = self.content.strip()
+					result += (str(count) + " en " + self.content + "\n")
+					self.content = ""
+					count += 1
+
+		return result
--- Python/XmlParser.py
+++ Python/XmlParser.py
@@ -1,8 +1,22 @@
 import os
 from xml.dom.minidom import parse
 import xml.dom.minidom
+from Parser import XmlParser
 
-if (__name__='__main__'):
+def writeToFile(filePath, content):
+	fp = open(filePath, 'w')
+	#content.encode('ascii', 'ignore')
+	fp.write(content.encode('ascii', 'ignore'))
+	fp.close()
+	return
+
+def parseXml(fileList):
+	rootTag = "article_rec"
+	targetTags = ["title", "par", "article_publication_date"]
+	parser = XmlParser(fileList, rootTag, targetTags)
+	return parser.parse()
+
+if (__name__=='__main__'):
 	dirList = os.listdir('.')
 	fileList = []
 	for fileName in dirList:
@@ -36,6 +50,15 @@ if (__name__='__main__'):
 			areaTitle = area
 			procTitle = proc
 
-	for item in wholeList:
-		print item
+	count = 0
+	for procs in wholeList:
+		#print item
+		for areas in procs:
+			parsedValues = ""
+			#doc = "data-" + areas[0].split('-')[0] + "-" + areas[0].split('-')[1] + ".txt"
+			#for fileName in areas:
+			doc = "testPy/data-" + str(count) + ".txt"
+			count += 1
+			parsedValues = parseXml(areas)
+			writeToFile(doc, parsedValues)
 	pass
