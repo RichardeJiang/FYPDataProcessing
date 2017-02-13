296c791a34553c353070742758f54fff373906b0
Richard Jiang Shenhao <richardejiang@hotmail.com>
2017-02-14 00:16:01 +0800

    commit for remote work

--- Python/linearRegression.py
+++ Python/linearRegression.py
@@ -26,10 +26,13 @@ def readTimeSeriesData(fileName):
 def splitData(timeSeries):
 	resultXList = []
 	resultYList = []
+	maxNum = 0.0
 	for phraseSeries in timeSeries:
 		resultX = []
 		resultY = []
 		phraseSeries = phraseSeries[:-4]
+		if max(phraseSeries) > maxNum:
+			maxNum = max(phraseSeries)
 		size = len(phraseSeries)
 		for i in range(size - 3):
 			resultX.append(phraseSeries[i: i+3])
@@ -37,6 +40,8 @@ def splitData(timeSeries):
 
 		resultXList.append(resultX)
 		resultYList.append(resultY)
+
+	print maxNum
 	return resultXList, resultYList
 
 if (__name__ == "__main__"):
@@ -64,8 +69,12 @@ if (__name__ == "__main__"):
 		meanSquareError[phraseList[index]] = np.mean((regression.predict(XTest) - YTest) ** 2)
 		varianceScore[phraseList[index]] = regression.score(XTest, YTest)
 
+		coef = regression.coef_
+
 		meanSquareErrorRidge[phraseList[index]] = np.mean((ridge.predict(XTest) - YTest) ** 2)
 		varianceScoreRidge[phraseList[index]] = ridge.score(XTest, YTest)
+
+		coefRidge = ridge.coef_
 
 		plt.scatter(regression.predict(XTrain), regression.predict(XTrain) - YTrain, color = 'b', s =40, alpha = 0.5)
 		plt.scatter(regression.predict(XTest), regression.predict(XTest) - YTest, color = 'g', s = 40)
@@ -82,6 +91,15 @@ if (__name__ == "__main__"):
 		plt.ylabel("Residuals")
 		plt.savefig("plots/" + str(index + 1) + "-" + str(phraseList[index]) + "-ridge.png")
 		plt.close()
+
+		print "Coef for linear: "
+		print coef
+
+		print "Coef for ridge: "
+		print coefRidge
+
+		if index == 50:
+			break
 
 	writeScore(meanSquareError, "data/meanLinear.txt")
 	writeScore(varianceScore, "data/varianceLinear.txt")
